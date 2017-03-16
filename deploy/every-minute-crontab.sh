9bc52d40e485fbe9984fd992a62bbe5765c4e30c
RenatoCRON <renato.cron@gmail.com>
2017-03-16 18:51:44 -0300

    add: test if process-requests is running

--- deploy/every-minute-crontab.sh
+++ deploy/every-minute-crontab.sh
@@ -7,9 +7,16 @@ source $HTTP_CB_ENV_FILE
 
 
 if /bin/fuser $HTTP_CB_API_PORT/tcp ; then
-    echo "voto legal is running"
+    echo "http callback is running"
 else
     cd $HTTP_CB_APP_DIR;
 
     HTTP_CB_ENV_FILE=deploy/envs.sh ./deploy/restart_services.sh
 fi
+
+if ps -ef | grep process-requests | grep -v 'grep'; then
+    HTTP_CB_ENV_FILE=deploy/envs.sh ./deploy/restart_services.sh
+else
+    echo "process-requests is running"
+fi
+
