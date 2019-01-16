0d744b60c8db3b93856c81cbd8dd076df0c0e732
Lucas Ansei <lucas.ansei@eokoe.com>
2019-01-16 15:50:37 -0200

    Updating docker scripts

--- restart-services.sh
+++ restart-services.sh
@@ -3,6 +3,6 @@ export PIDFILE=/tmp/start_server.pid;
 
 cd /src;
 
-MOJO_MODE=production MOJO_MAX_MESSAGE_SIZE=1073741824 LIBEV_FLAGS=4 MOJO_INACTIVITY_TIMEOUT=600 hypnotoad api-server
+LIBEV_FLAGS=4 hypnotoad api-server
 
 pgrep -f 'perl script/process-request' | xargs kill
--- start-server.sh
+++ start-server.sh
@@ -6,7 +6,8 @@ mkdir -p /data/log/;
 cd /src;
 source envfile.sh
 
-#sqitch deploy -t $HTTP_CB_SQITCH_DEPLOY_NAME 2>>/data/log/starman.log 1>&2
+sqitch deploy -t $HTTP_CB_SQITCH_DEPLOY_NAME 2>>/data/log/starman.log 1>&2
 
-MOJO_MODE=production MOJO_MAX_MESSAGE_SIZE=1073741824 LIBEV_FLAGS=4 MOJO_INACTIVITY_TIMEOUT=600 hypnotoad api-server
+LIBEV_FLAGS=4 hypnotoad api-server
 
+sleep infinity
